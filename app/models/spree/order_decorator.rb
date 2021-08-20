df333d866848da3fc708b8a555486d3c632bdc79
Dimitrios Panagkasidis <dimidev@olympusone.com>
2021-08-20 22:34:14 +0300

    Update order_decorator.rb

--- app/models/spree/order_decorator.rb
+++ app/models/spree/order_decorator.rb
@@ -1,6 +1,6 @@
 module Spree::OrderDecorator
     def cash_on_delivery?
-        payments.where(state: ['checkout', 'pending']).map { |m| m.payment_method.type }.include? "Spree::PaymentMethod::CashOnDelivery"
+        payments.where.not(state: ['failed', 'void']).map { |m| m.payment_method.type }.include? "Spree::PaymentMethod::CashOnDelivery"
     end
 end
   
