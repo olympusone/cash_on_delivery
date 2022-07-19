942e1c93755d8a36a1ee6bd7e1daafdbf9b85a61
dimidev <dimidev@olympusone.com>
2022-07-19 13:09:32 +0300

    u

--- lib/spree_cash_on_delivery/engine.rb
+++ lib/spree_cash_on_delivery/engine.rb
@@ -1,33 +1,28 @@
-module SpreeOrderComments
+module SpreeCashOnDelivery
   class Engine < Rails::Engine
     require 'spree/core'
     isolate_namespace Spree
-    engine_name 'spree_order_comments'
-
+    engine_name 'spree_cash_on_delivery'    
+    
     # use rspec for tests
     config.generators do |g|
       g.test_framework :rspec
     end
 
-    config.after_initialize do
-      Spree::PermittedAttributes.checkout_attributes << :comments
-      Spree::PermittedAttributes.line_item_attributes << :comments
-
-      Spree::Api::Dependencies.storefront_cart_serializer = 'CartSerializer'
-
-      config.spree.line_item_comparison_hooks.add 'compare_line_item_comments'
+    initializer 'spree.register.payment_methods', after: :after_initialize do |_app|
+      _app.config.spree.payment_methods << Spree::PaymentMethod::CashOnDelivery
     end
 
-    initializer 'spree_order_comments.environment', before: :load_config_initializers do |_app|
-      SpreeOrderComments::Config = SpreeOrderComments::Configuration.new
+    initializer 'spree_cash_on_delivery.environment', before: :load_config_initializers do |_app|
+      SpreeCashOnDelivery::Config = SpreeCashOnDelivery::Configuration.new
     end
 
     def self.activate
-      Dir.glob(File.join(File.dirname(__FILE__), '../../app/**/*_decorator*.rb')) do |c|
-        Rails.configuration.cache_classes ? require(c) : load(c)
+      Dir.glob(File.join(File.dirname(__FILE__), '../../app/**/spree/*_decorator*.rb')) do |c|
+        Rails.application.config.cache_classes ? require(c) : load(c)
       end
     end
-
+    
     config.to_prepare(&method(:activate).to_proc)
   end
 end
